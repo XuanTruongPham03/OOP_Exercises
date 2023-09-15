b933a60a710c7698ce6a48315d7110bfa2db7a10
phamxuantruong843 <21020417@vnu.edu.vn>
2023-09-15 19:35:36 +0700

    Init

---
+++ src/Week2/primeNumber/Solution.java
@@ -0,0 +1,40 @@
+package Week2.primeNumber;
+
+import java.util.Scanner;
+
+public class Solution {
+    /**
+     * Phương thức isPrime kiểm tra số nguyên tố.
+     * @param n Số tự nhiên.
+     * @result true nếu n là số nguyên tố hoặc false nếu không.
+     */
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Hàm thực thi main.
+     * @param args Tham số dòng lệnh (Không sử dụng).
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        scanner.close();
+        if (isPrime(n)) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
+}
